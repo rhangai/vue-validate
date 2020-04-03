26b39a10a56aa8b6ee5cb5acb2a8e6b0322f9f6d
rhangai <hangai.renan@gmail.com>
2020-04-03 20:07:36 -0300

    chore: Updated vuelidate directive

--- src/directive/ValidateDirectiveVuelidate.ts
+++ src/directive/ValidateDirectiveVuelidate.ts
@@ -3,43 +3,56 @@ import { ValidateManager } from "../manager/ValidateManager";
 import { BehaviorSubject, Observable, combineLatest } from "rxjs";
 import { map } from "rxjs/operators";
 import { ValidateRulesManager } from "../rules/ValidateRulesManager";
+import { ValidateComponent } from "../manager/ValidateComponent";
+
+class ValidateDirectiveVuelidateManager {
+	private vuelidate: any;
+	private rulesManager = new ValidateRulesManager();
+	private validateComponent: ValidateComponent | null;
+
+	constructor(component: Vue | undefined | null) {
+		this.validateComponent = ValidateManager.create(component, {
+			reset: this.reset.bind(this),
+			validate: this.validate.bind(this),
+			state$: (component: Vue) =>
+				this.rulesManager.fromComponent$(component),
+		});
+	}
+
+	destroy() {
+		this.validateComponent?.destroy();
+	}
+
+	setBinding(binding: any) {
+		if (this.vuelidate !== binding.value) {
+			this.vuelidate = binding.value;
+			this.rulesManager.setRules(vuelidateRules(binding.value));
+		}
+	}
+
+	validate() {
+		this.vuelidate?.$touch?.();
+	}
+
+	reset() {
+		this.vuelidate?.$reset?.();
+	}
+}
 
 export const ValidateDirectiveVuelidate: DirectiveOptions = {
 	bind(el: any, binding, vnode) {
 		const dirty$ = new BehaviorSubject(false);
-		const component = vnode.componentInstance;
-
-		const rulesManager = new ValidateRulesManager(
-			vuelidateRules(binding.value)
+		const manager = new ValidateDirectiveVuelidateManager(
+			vnode.componentInstance
 		);
-		el.validateRulesManager = rulesManager;
-		el.validateComponent = ValidateManager.create(component, {
-			reset() {
-				dirty$.next(false);
-			},
-			validate() {
-				dirty$.next(true);
-			},
-			state$(component: Vue) {
-				return combineLatest([
-					rulesManager.fromComponent$(component),
-					dirty$,
-				]).pipe(
-					map(([isValid, isDirty]) => {
-						// if (!isDirty) return true;
-						return !isValid;
-					})
-				);
-			},
-		});
+		manager.setBinding(binding);
+		el.validateDirectiveManager = manager;
 	},
 	update(el: any, binding) {
-		el.validateRulesManager.setRules(vuelidateRules(binding.value));
+		el.validateDirectiveManager?.setBinding(binding);
 	},
 	unbind(el: any, binding, vnode) {
-		if (el.validateComponent) {
-			el.validateComponent.destroy();
-		}
+		el.validateDirectiveManager?.destroy();
 	},
 };
 
